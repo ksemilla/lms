b4c89a8fdd08d12ecdb8cd5af841a695bee7116e
Kevin Semilla <kevinsemilla@gmail.com>
2023-11-07 13:36:52 +1100

    Change home page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -159,22 +159,15 @@ export function Home() {
                   <div className="rounded-xl overflow-hidden inline-block">
                     <img src={logo1} />
                   </div>
-                  <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                  <h1 className="text-xl text-center font-bold tracking-tight text-gray-900 sm:text-2xl">
                     We’re changing the way students connect.
                   </h1>
-                  <p className="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
-                    Cupidatat minim id magna ipsum sint dolor qui. Sunt sit in
-                    quis cupidatat mollit aute velit. Et labore commodo nulla
-                    aliqua proident mollit ullamco exercitation tempor. Sint
-                    aliqua anim nulla sunt mollit id pariatur in voluptate
-                    cillum.
-                  </p>
-                  <div className="mt-10 flex items-center gap-x-6">
+                  <div className="mt-10 flex items-center justify-center gap-x-6">
                     <Link
                       to="/login"
                       className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
-                      Get started
+                      Log in
                     </Link>
                   </div>
                 </div>
